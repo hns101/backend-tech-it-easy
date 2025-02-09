e0dd0cde1c01b301834a48efdbf604e75cb90df5
Hans Bregman <hns101@live.nl>
2025-02-09 14:11:24 +0100

    Opdracht gemaakt aleen nog niet de Bonus bonus

--- src/main/java/nl/scheveschilder/techiteasybackend/controllers/ExceptionController.java
+++ src/main/java/nl/scheveschilder/techiteasybackend/controllers/ExceptionController.java
@@ -1,4 +1,23 @@
 package nl.scheveschilder.techiteasybackend.controllers;
 
+import nl.scheveschilder.techiteasybackend.exceptions.RecordNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.ErrorResponse;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class ExceptionController {
+    @ExceptionHandler(value = RecordNotFoundException.class)
+    public ResponseEntity<String> recordNotFoundException(RecordNotFoundException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
+    @ExceptionHandler(value = IndexOutOfBoundsException.class)
+    public ResponseEntity<String> fetchIndexOutOfBoundsException(IndexOutOfBoundsException exception) {
+        return new ResponseEntity<>(exception.getMessage(), HttpStatus.NOT_FOUND);
+    }
 }
--- src/main/java/nl/scheveschilder/techiteasybackend/controllers/TelevisionsController.java
+++ src/main/java/nl/scheveschilder/techiteasybackend/controllers/TelevisionsController.java
@@ -1,6 +1,57 @@
 package nl.scheveschilder.techiteasybackend.controllers;
 
+import nl.scheveschilder.techiteasybackend.exceptions.RecordNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@RestController
+@RequestMapping("/tv")
 public class TelevisionsController {
+
+    private List<String> televisionDataBase = new ArrayList<>();
+
+    @GetMapping("/getall")
+    public ResponseEntity<List<String>> getTelevisions(){
+        if (televisionDataBase.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return ResponseEntity.ok(televisionDataBase);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<String> getTelevisionById(@PathVariable int id) {
+        if (id == 4) {
+            throw new RecordNotFoundException("Deze Televisie Id " + id + " bestaat niet 😭");
+        }
+        if (id == 6) {
+            throw new RecordNotFoundException(id);
+        }
+        return ResponseEntity.ok(televisionDataBase.get(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<String> addTelevision(@RequestBody String tv) {
+        televisionDataBase.add(tv);
+        return ResponseEntity.created(null).body("Televisie " + tv + " aangemaakt");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateTelevision(@PathVariable int id, @RequestBody String tv) {
+        televisionDataBase.set(id, tv);
+        return ResponseEntity.ok("Televisie " + tv + " aangepast op Id" + id);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTelevision(@PathVariable int id) {
+        televisionDataBase.set(id,null);
+        return ResponseEntity.noContent().build();
+    }
+
+
 
 }
